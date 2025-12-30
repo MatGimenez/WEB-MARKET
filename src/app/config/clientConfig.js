3d1dd554690eef0e6e906096ed52ed30e51170c9
Matias Gimenez <p.matias.gimenez@hotmail.es>
2025-12-30 08:46:10 -0300

    Initial commit

---
+++ src/app/config/clientConfig.js
@@ -0,0 +1,8 @@
+export const clientConfig = {
+  brandName: "Hola Colaciones",
+  tagline: "Pizzas, pastas, postres y bebidas sin alcohol",
+  currency: "CLP", // "CLP" | "USD" | "EUR"
+  whatsappNumber: "56912345678", // solo dígitos
+  deliveryFee: 1500,
+  minOrder: 0,
+};
